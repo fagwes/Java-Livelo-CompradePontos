d705008fc18dcecfb02d6afd7464dc6653769bbb
fagwes <109398136+fagwes@users.noreply.github.com>
2022-12-06 17:02:54 -0300

    Update Metodos.java

--- Metodos.java
+++ Metodos.java
@@ -1,6 +1,12 @@
 package metodos;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import drivers.Drivers;
 
@@ -15,5 +21,12 @@ public class Metodos extends Drivers {
 
 		driver.findElement(elementos).click();
 	}
+	public static void evidenciarteste(String nomeEvidencia) throws IOException {
 
+		TakesScreenshot scrShot = (TakesScreenshot) driver;
+		File scrFile = scrShot.getScreenshotAs(OutputType.FILE);
+		File destFile = new File("./evidencias/" + nomeEvidencia + ".png");
+		FileUtils.copyFile(scrFile, destFile);
+
+	}
 }
