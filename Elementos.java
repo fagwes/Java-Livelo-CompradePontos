7e68b9faa72c186a741041954fc525185c16fee0
fagwes <109398136+fagwes@users.noreply.github.com>
2022-12-08 21:58:32 -0300

    Add files via upload

---
+++ Elementos.java
@@ -0,0 +1,21 @@
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class Elementos {
+
+	public By comprepontos = By.xpath("//*[@id=\"span-first-level-menu-4\"]");
+	public By paravoce = By.xpath("//*[@id=\"span-second-level-menu-4-1\"]");
+	public By pontoscomprar = By.xpath("//*[@id=\"input-quantity\"]");
+	public By inserircupom = By.cssSelector("#wi8200106-discount-coupon-id > div > input");
+	public By comprarpontos = By.xpath("//*[@id=\"buy-btn\"]");
+
+	public By paraumamigo = By.xpath("//*[@id=\"span-second-level-menu-4-2\"]");
+	public By cpf = By.xpath("//*[@id=\"cpf\"]");
+	public By comprarpontosnovamente = By.xpath("//*[@id=\"buy-btn\"]");
+
+	public By mais = By.xpath("//*[@id=\"wi8200106-quantity-input-id\"]/form/div[2]/a[2]");
+
+	public By cookiesautorizar = By.xpath("//*[@id=\"btn-authorizeCoookies\"]");
+
+}
